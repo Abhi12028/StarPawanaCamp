6025e0de6f2a35fbc1dce29c89b80b02668eda0b
Abhishek <abhisutar12028@gmail.com>
2024-11-30 18:57:53 +0530

    new depoly

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: '/starpawanacamp/', // Replace 'starpawanacamp' with your repository name
-});
+  plugins: [react()],
+  base: "/book-landing-page/"
+})
